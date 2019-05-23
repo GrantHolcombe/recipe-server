a422cb1a452d52d1e8ba4e327552adb9782c1291
Grant Holcombe <grant.holcombe@missionbaymedia.com>
2019-05-23 15:00:26 -0700

    uri update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0dab627279c28bd5489542ea08f32687.js"
+  "/precache-manifest.d18710a8b49a653ac20f9f10778089eb.js"
 );
 
 self.addEventListener('message', (event) => {
