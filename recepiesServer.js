8b892d6bfed103a3bc7bae982166388cadca2768
Grant Holcombe <grant.holcombe@missionbaymedia.com>
2019-05-23 15:04:29 -0700

    uri update

--- recepiesServer.js
+++ recepiesServer.js
@@ -50,7 +50,7 @@ var BodyParser = require("body-parser")
     app = express(),
     port = process.env.PORT || 3000;
 
-    app.use(cors({origin: 'http://13.57.244.99:3000'}));
+    app.use(cors({origin: 'http://ec2-13-57-244-99.us-west-1.compute.amazonaws.com:3000/'}));
     app.use(BodyParser.json());
     app.use(BodyParser.urlencoded({ extended: true }));
 
